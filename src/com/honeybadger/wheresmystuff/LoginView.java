05a51e69dc6912d607c037372e5f170887bd3c01
stange.anthony <stange.anthony@gmail.com>
2013-02-19 23:34:25 +0000

    Cleared up some clutter in LoginView and deleted LoginActivity

--- src/com/honeybadger/wheresmystuff/LoginView.java
+++ src/com/honeybadger/wheresmystuff/LoginView.java
@@ -4,11 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class LoginView extends Activity{
 	String email;
@@ -37,19 +34,6 @@ public class LoginView extends Activity{
 		mEmailView.setText(email);
 		
 		mPasswordView = (EditText) findViewById(R.id.password);
-		mPasswordView
-				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-					@Override
-					public boolean onEditorAction(TextView textView, int id,
-							KeyEvent keyEvent) {
-						if (id == R.id.login || id == EditorInfo.IME_NULL) {
-							attemptLogin();
-							return true;
-						}
-						return false;
-					}
-				});
-
 		
 		findViewById(R.id.sign_in_button).setOnClickListener(
 				new View.OnClickListener() {
