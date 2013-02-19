f3e7c6f7c8773aa897af97d3a537cde82ce6f025
stange.anthony@gmail.com <stange.anthony@gmail.com@03b83ac3-6690-9d20-bea0-5b52570744bb>
2013-02-19 23:17:56 +0000

    Fixed Bug

--- trunk/src/com/honeybadger/wheresmystuff/LoginView.java
+++ trunk/src/com/honeybadger/wheresmystuff/LoginView.java
@@ -1,12 +1,12 @@
 package com.honeybadger.wheresmystuff;
 
-import com.honeybadger.wheresmystuff.LoginActivity.UserLoginTask;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,9 +17,6 @@ public class LoginView extends Activity{
 	// UI references.
 	private EditText mEmailView;
 	private EditText mPasswordView;
-	private View mLoginFormView;
-	private View mLoginStatusView;
-	private TextView mLoginStatusMessageView;
 	
 	public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
 	
@@ -38,6 +35,21 @@ public class LoginView extends Activity{
 		email = getIntent().getStringExtra(EXTRA_EMAIL);
 		mEmailView = (EditText) findViewById(R.id.email);
 		mEmailView.setText(email);
+		
+		mPasswordView = (EditText) findViewById(R.id.password);
+		mPasswordView
+				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+					@Override
+					public boolean onEditorAction(TextView textView, int id,
+							KeyEvent keyEvent) {
+						if (id == R.id.login || id == EditorInfo.IME_NULL) {
+							attemptLogin();
+							return true;
+						}
+						return false;
+					}
+				});
+
 		
 		findViewById(R.id.sign_in_button).setOnClickListener(
 				new View.OnClickListener() {
