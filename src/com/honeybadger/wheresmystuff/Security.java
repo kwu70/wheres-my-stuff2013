f397236b45cf5fed7dcfe62d0d55e355967729a2
stange.anthony <stange.anthony@gmail.com>
2013-02-19 23:06:39 +0000

    Started adding base classes and background classes

---
+++ src/com/honeybadger/wheresmystuff/Security.java
@@ -0,0 +1,15 @@
+package com.honeybadger.wheresmystuff;
+
+import java.util.ArrayList;
+
+public class Security {
+	protected ArrayList<String> emails = new ArrayList<String>();
+	protected ArrayList<String> passwords = new ArrayList<String>();
+	public Security(){
+		emails.add("foo@example.com");
+		passwords.add("Hello");
+	}
+	
+	
+	
+}
