e4a2bf00bf5991b7a04d7f86b0abec7d271125a1
jelliott36@gatech.edu <jelliott36@gatech.edu@03b83ac3-6690-9d20-bea0-5b52570744bb>
2013-02-26 23:24:39 +0000

    Security Updates Removed emails and passwords lists Made members private static Added more functionality - getter for members list - getter for members.size() - getter for a Member within members list (Based on email) - contains method (Determines if a Member is in the list (Based on email)) - addMember method adds a Member to the members list (takes in email and password) (no return) - removeMember method removes a Member from the list based on email. (returns boolean if successful)
    May add more classes.

--- trunk/src/com/honeybadger/wheresmystuff/support/Security.java
+++ trunk/src/com/honeybadger/wheresmystuff/support/Security.java
@@ -2,22 +2,95 @@ package com.honeybadger.wheresmystuff.support;
 
 import java.util.ArrayList;
 /**
- * The Security class contains email and password lists with which user
- * input can be checked.
+ * The Security class contains member list which contains all the members that exist in the system.
  */
 public class Security {
-	protected ArrayList<String> emails = new ArrayList<String>();
-	protected ArrayList<String> passwords = new ArrayList<String>();
 	
-	public static ArrayList<Member> members = new ArrayList<Member>();
+	private static ArrayList<Member> members = new ArrayList<Member>();
 	
 	/**
 	 * Security constructor that sets up the ArrayLists emails, passwords, and members
 	 * it initiates them with default emails and passwords
 	 */
 	public Security(){
-		emails.add("foo@example.com");
-		passwords.add("hello");
 		members.add(new Member("example@example.com", "example"));
 	}
+	
+	/**
+	 * Getter for the members list.
+	 * 
+	 * @return members the list that contains all the members
+	 */
+	public static ArrayList<Member> getMemberList(){
+		return members;
+	}
+	
+	/**
+	 * Getter for member list Size
+	 *  
+	 * @return members.size() an int indicating the size of the list.
+	 */
+	public static int getMemberListSize(){
+		return members.size();
+	}
+	
+	/**
+	 * Searches through the list of all members for the member with the specified email
+	 * 
+	 * @param email Email of the specific member
+	 * @return Member with the email. If the email is not found then return default Member.
+	 */
+	public static Member getMember(String email){
+		for(int i = 0; i < Security.members.size(); i++){
+			if(email.equals(Security.members.get(i).getEmail())){
+				return members.get(i);
+			}
+		}
+		return new Member();
+	}
+	
+	/**
+	 * Determines if a member is in the list based on the email
+	 * 
+	 * @param email Email of specific member
+	 * @return boolean based on whether the Member is in the list.
+	 */
+	public static boolean contains(String email){
+		for(int i = 0; i < Security.members.size(); i++){
+			if(email.equals(Security.members.get(i).getEmail())){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Adds a new Member to the member list. 
+	 * 
+	 * NOTE: MAKE SURE THAT THE EMAIL DOES NOT ALREADY EXIST BEFORE CALLING THIS METHOD
+	 * YOU CAN DO THIS USING CONTAINS METHOD
+	 * 
+	 * @param email of the Member being added to the member list
+	 * @param password  of the Member being added to the member list
+	 */
+	public static void addMember(String email, String password){
+		members.add(new Member(email, password));
+	}
+	
+	/**
+	 * Remove a Member from the member list based on email.
+	 * 
+	 * @param email
+	 * @param password
+	 */
+	public static boolean removeMember(String email){
+		for(int i = 0; i < Security.members.size(); i++){
+			if(email.equals(Security.members.get(i).getEmail())){
+				members.remove(i);
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
