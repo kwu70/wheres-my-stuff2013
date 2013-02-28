09daf603bd2a50d6348bb9800eeb0e9a37f3e088
jalex618 <jalex618@gmail.com>
2013-02-28 09:12:57 +0000

    Updated to work with other classes. Security object kept in the constructor even though not used.

--- src/com/honeybadger/wheresmystuff/support/Login.java
+++ src/com/honeybadger/wheresmystuff/support/Login.java
@@ -2,61 +2,56 @@ package com.honeybadger.wheresmystuff.support;
 
 /**
  * The Login class is responsible for verifying login credentials, logging
- * a user in if they exist, locking a user out if they are incorrect, and 
- * creating an account if the email does not yet exist.
+ * a user in if they exist, locking a user out if they enter incorrect information
+ * three times, and creating an account if the email does not yet exist.
  */
 public class Login {
-	private Security sc;
 	
+	private Security sc;
+
 	/**
 	 * Login constructor that creates a new Security object.
 	 */
 	public Login(){
 		sc = new Security();
 	}
-	
+
 	/**
-	 * Prevents user from logging in.
-	 *
-	 * @return 		true if user is locked out, false otherwise
+	 * Prevents user from logging in if login attempts equal to 3.
+	 * 
+	 * @param email Email of the specific member.
+	 * @return boolean True if user is locked out, false otherwise.
 	 */
-	public boolean lockOut(){
-		return false;
+	public boolean lockOut(Member member){
+		return member.getFailedAttempts() == 3;
 	}
-	
+
 	/**
-	 * Checks if member is part of the system using the email inputted by the user.
-	 * If email and password for the email are correct then it will return true
-	 * Otherwise it returns false
+	 * Checks if member is part of the system using the email inputed by the user.
+	 * If email and password for the email are correct, then it will return true.
+	 * Otherwise, it returns false.
 	 *
-	 * @param email User's inputted email address
-	 * @param psswd User's inputted password
-	 * @return 		whether combination of login credentials exists or not 
+	 * @param email User's inputed email address.
+	 * @param psswd User's inputed password.
+	 * @return boolean Whether combination of login credentials exists or not. 
 	 */
 	public boolean validate(String email, String password){
-		int index = 0;
-		for(int i = 0; i < Security.members.size(); i++){
-			if(email.equals(Security.members.get(i).getEmail())){
-				index = i;
+
+		if(Security.getMemberList().contains(email)){
+			if(password.equals(Security.getMember(email).getPassword())
+						&& lockOut(Security.getMember(email)) == false){
+				Security.getMember(email).setFailedAttempts(0);
+				return true;
+			}
+			else{
+				Security.getMember(email).incFailedAttempts();
+				return false;
 			}
 		}
-		if(password.equals(Security.members.get(index).getPassword())){
-			return true;
-		}
-			return false;
+		// Else, email string is available and user can be prompted to register
+		// with new email.
+		return false;
 	}
-	
-	/**
-	 * Adds user credentials to email and password ArrayLists.
-	 *
-	 * @param email User's inputted email address
-	 * @param psswd User's inputted password
-	 * @return 		true boolean value
-	 */
-	public boolean createAccount(String email, String psswd) {
-		sc.emails.add(email);
-		sc.passwords.add(psswd);
-		Security.members.add(new Member(email, psswd));
-		return true;
-	}
+
+
 }
