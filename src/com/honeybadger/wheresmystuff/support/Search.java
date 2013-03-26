def2836f97a9dc1a512c9156eb59593dd118403f
jalex618 <jalex618@03b83ac3-6690-9d20-bea0-5b52570744bb>
2013-03-26 22:42:26 +0000

    Fixed some of the filters.

--- src/com/honeybadger/wheresmystuff/support/Search.java
+++ src/com/honeybadger/wheresmystuff/support/Search.java
@@ -10,7 +10,7 @@ public class Search {
 		ArrayList<Item> matches = new ArrayList<Item>();
 		for(int i = 0; i < Security.getItemListSize(); i++){
 			Item currItem = list.get(i);
-			if(currItem.getType().equals(currItem.getType())){
+			if(currItem.getType().equals(category)){
 				matches.add(list.get(i));
 			}
 		}
@@ -21,11 +21,29 @@ public class Search {
 		
 		ArrayList<Item> list = Security.getItemList();
 		ArrayList<Item> matches = new ArrayList<Item>();
+		
+		String[] mdy = date.split("/");
+		int month = Integer.parseInt(mdy[0]);
+		int day = Integer.parseInt(mdy[1]);
+		int year = Integer.parseInt(mdy[2]);
 		for(int i = 0; i < Security.getItemListSize(); i++){
 			Item currItem = list.get(i);
-			if(currItem.getDay() == currItem.getDay()
-					&& currItem.getMonth() == currItem.getMonth()
-					&& currItem.getYear() == currItem.getYear()){
+			if(currItem.getDay() == month
+					&& currItem.getMonth() == day
+					&& currItem.getYear() == year){
+				matches.add(list.get(i));
+			}
+		}
+		return matches;
+	}
+	public static ArrayList<Item> filterStatus(Member member, Boolean status){
+		
+		
+		ArrayList<Item> list = Security.getItemList();
+		ArrayList<Item> matches = new ArrayList<Item>();
+		for(int i = 0; i < Security.getItemListSize(); i++){
+			Item currItem = list.get(i);
+			if(currItem.getStatus() == status){
 				matches.add(list.get(i));
 			}
 		}
