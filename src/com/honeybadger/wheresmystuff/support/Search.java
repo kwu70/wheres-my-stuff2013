0821d7654f4ab094f46e939579ddb5a541e9935a
jalex618 <jalex618@03b83ac3-6690-9d20-bea0-5b52570744bb>
2013-03-27 20:08:34 +0000

    Changed and commented filter methods.

--- src/com/honeybadger/wheresmystuff/support/Search.java
+++ src/com/honeybadger/wheresmystuff/support/Search.java
@@ -3,16 +3,23 @@ import java.util.ArrayList;
 
 public class Search {
 	
+	/**
+	 * Filters member's items to show only those that have a given category
+	 * 
+	 * @param member A member
+	 * @param category String representing category
+	 * @return matches the list that contains items of specified category
+	 */
 	public static ArrayList<Item> filterCategory(Member member, String category){
 		ArrayList<Item> matches = new ArrayList<Item>();
 
-		if((category == null) || (category.length() == 0)) {
-			
+		if((category == null) && (category.length() == 0)) {
+			return null;
 		}
 		else{
-			ArrayList<Item> list = Security.getMemberItemList(member);
+			ArrayList<Item> list = member.getItems();
 			
-			for(int i = 0; i < Security.getItemListSize(); i++){
+			for(int i = 0; i < list.size(); i++){
 				Item currItem = list.get(i);
 				if(currItem.getType().equals(category)){
 					matches.add(list.get(i));
@@ -22,20 +29,27 @@ public class Search {
 		return matches;
 	}
 	
+	/**
+	 * Filters member's items to show only those that were posted as lost on a given date
+	 * 
+	 * @param member A member
+	 * @param date String representing date
+	 * @return matches the list that contains items lost on specified date
+	 */
 	public static ArrayList<Item> filterDate(Member member, String date){
 		
 		if((null != date) && (date.length() == 0)) {
-			throw new NullPointerException("Date is null.");
+			return null;
 		}
 		
-		ArrayList<Item> list = Security.getMemberItemList(member);
+		ArrayList<Item> list = member.getItems();
 		ArrayList<Item> matches = new ArrayList<Item>();
 		
 		String[] mdy = date.split("/");
 		int month = Integer.parseInt(mdy[0]);
 		int day = Integer.parseInt(mdy[1]);
 		int year = Integer.parseInt(mdy[2]);
-		for(int i = 0; i < Security.getItemListSize(); i++){
+		for(int i = 0; i < list.size(); i++){
 			Item currItem = list.get(i);
 			if(currItem.getMonth() == month
 					&& currItem.getDay() == day
@@ -45,12 +59,20 @@ public class Search {
 		}
 		return matches;
 	}
+	
+	/**
+	 * Filters member's items to show only those that have a matching status
+	 * 
+	 * @param member A member
+	 * @param status Boolean representing lost or found status
+	 * @return matches the list that contains items with specified status
+	 */
 	public static ArrayList<Item> filterStatus(Member member, Boolean status){
 		
-		ArrayList<Item> list = Security.getMemberItemList(member);
+		ArrayList<Item> list = member.getItems();
 		ArrayList<Item> matches = new ArrayList<Item>();
 		
-		for(int i = 0; i < Security.getItemListSize(); i++){
+		for(int i = 0; i < list.size(); i++){
 			Item currItem = list.get(i);
 			if(currItem.getStatus() == status){
 				matches.add(list.get(i));
