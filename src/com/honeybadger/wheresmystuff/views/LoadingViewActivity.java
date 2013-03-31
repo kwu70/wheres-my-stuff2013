6b1e8929bbc252a48ca0c1cd840965829ff93e8d
thewilsonkid <thewilsonkid@03b83ac3-6690-9d20-bea0-5b52570744bb>
2013-03-31 23:42:15 +0000

    loading screen activity

---
+++ src/com/honeybadger/wheresmystuff/views/LoadingViewActivity.java
@@ -0,0 +1,33 @@
+package com.honeybadger.wheresmystuff.views;
+
+import com.honeybadger.wheresmystuff.R;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class LoadingViewActivity extends Activity{
+	//Called when activity is first created
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		final ProgressDialog pd = new ProgressDialog(this);
+		pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		pd.setIndeterminate(true);
+		pd.setCancelable(true);
+		
+		Button b = (Button) findViewById(R.id.sign_in_button);
+		b.setOnClickListener(new OnClickListener(){
+		
+			@Override
+			public void onClick(View arg0){
+				pd.show();
+			}
+		});
+	}
+}
+		
