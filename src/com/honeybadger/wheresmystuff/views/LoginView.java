0819865205f019490b9f48cce9e91fbc5f762a7c
thewilsonkid <thewilsonkid@03b83ac3-6690-9d20-bea0-5b52570744bb>
2013-04-02 20:40:33 +0000

    edit to Loading class

--- src/com/honeybadger/wheresmystuff/views/LoginView.java
+++ src/com/honeybadger/wheresmystuff/views/LoginView.java
@@ -33,9 +33,7 @@ public class LoginView extends Activity{
 	
 	//Intent to switch to RegisterActivity
 	private Intent registerIntent;
-		
-	//private Intent load;
-	
+			
 	private Login lg;
 
 	/**
@@ -52,8 +50,6 @@ public class LoginView extends Activity{
 		
 		memberIntent = new Intent(this, MemberActivity.class);		
 		registerIntent = new Intent(this, RegisterActivity.class);
-		//load = new Intent(this, Loading.class);
-		//startActivity(load);
 		
 		//login initialized so we can access methods and it sets up a user account since
 		//we do not have persistent data.
