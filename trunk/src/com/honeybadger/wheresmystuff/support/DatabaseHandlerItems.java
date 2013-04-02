e409a6ab3003e02d2b3dcd4deb873e114ccde308
stange.anthony@gmail.com <stange.anthony@gmail.com@03b83ac3-6690-9d20-bea0-5b52570744bb>
2013-04-02 23:19:20 +0000

    I code audited DatabaseHandlerItems, DatabaseHandlerMembers, and Member.java

--- trunk/src/com/honeybadger/wheresmystuff/support/DatabaseHandlerItems.java
+++ trunk/src/com/honeybadger/wheresmystuff/support/DatabaseHandlerItems.java
@@ -8,6 +8,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+/**
+ * This class is used to manage the database for items
+ * @author - TheHoneyBadgers
+ * @version - 1
+ */
+
 public class DatabaseHandlerItems extends SQLiteOpenHelper {
 
 	// All Static variables
@@ -22,35 +28,54 @@ public class DatabaseHandlerItems extends SQLiteOpenHelper {
 	
 	// Table Item Columns names
 	private static final String KEY_IDI = "itemID";
+	
 	private static final String KEY_INAME = "name";
+	
 	private static final String KEY_DESC = "description";
+	
 	private static final String KEY_MEMID = "memberid";
+	
 	private static final String KEY_STATUS = "itemstatus";
+	
 	private static final String KEY_RESOL = "resolved";
+	
 	private static final String KEY_TYPE = "type";
+	
 	private static final String KEY_MONTH = "month";
+	
 	private static final String KEY_DAY = "day";
+	
 	private static final String KEY_YEAR = "year";
+	
 	private static final String KEY_LOC = "location";
 
-
+	/**
+	 * Creates the database
+	 */
 	public DatabaseHandlerItems(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
-	// Creating Tables
+	/**
+	 * (non-Javadoc)
+	 * @see android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase)
+	 */
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		//creating tables
-		String CREATE_ITEMS_TABLE = "CREATE TABLE " + TABLE_ITEMS + "("
+		final String CREATE_ITEMS_TABLE = "CREATE TABLE " + TABLE_ITEMS + "("
 				+ KEY_IDI + " INTEGER PRIMARY KEY," + KEY_INAME + " TEXT,"
 				+ KEY_DESC + " TEXT, " + KEY_MEMID  + " TEXT," + KEY_STATUS 
 				+ " TEXT," + KEY_RESOL + " TEXT," + KEY_TYPE + " TEXT," + KEY_MONTH
-				+ " TEXT," + KEY_DAY + " TEXT," + KEY_YEAR + " TEXT," + KEY_LOC + " TEXT" + ")";
+				+ " TEXT," + KEY_DAY + " TEXT," + KEY_YEAR + " TEXT," + KEY_LOC 
+				+ " TEXT" + ")";
 		db.execSQL(CREATE_ITEMS_TABLE);
 	}
 
-	// Upgrading database
+	/**
+	 * (non-Javadoc)
+	 * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
+	 */
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// Drop older table if existed
@@ -63,10 +88,12 @@ public class DatabaseHandlerItems extends SQLiteOpenHelper {
 	/**
 	 * All CRUD(Create, Read, Update, Delete) Operations
 	 */
-
 	
-	// Adding new item
-	void addItem(Item item) {
+	/**
+	 * takes all item values and stores them into the database
+	 * @param item - item being added to the database
+	 */
+	public void addItem(Item item) {
 		SQLiteDatabase db = this.getWritableDatabase();
 		
 		ContentValues values = new ContentValues();
@@ -75,17 +102,17 @@ public class DatabaseHandlerItems extends SQLiteOpenHelper {
 		values.put(KEY_DESC, item.getDescription());
 		values.put(KEY_MEMID, item.getOwner().getID());
 		//Adding appropriate Values
-		if(item.getStatus() == false){
+		if(!item.getStatus()){
 			values.put(KEY_STATUS, "false");
 		}
 		else{
 			values.put(KEY_STATUS, "true");
 		}
-		if(item.getResolved() == false){
-			values.put(KEY_RESOL, "false");	
+		if(!item.getResolved()){
+			values.put(KEY_RESOL, "false");
 		}
 		else{
-			values.put(KEY_RESOL, "true");	
+			values.put(KEY_RESOL, "true");
 		}
 		values.put(KEY_TYPE, item.getType());
 		values.put(KEY_MONTH, item.getMonth());
@@ -97,18 +124,28 @@ public class DatabaseHandlerItems extends SQLiteOpenHelper {
 		db.insert(TABLE_ITEMS, null, values);
 		db.close(); // Closing database connection
 	}
-
 	
-	Item getItem(int id) {
+	/**
+	 * @param id - item that is being retrieved from the database
+	 */
+	public Item getItem(int id) {
 		SQLiteDatabase db = this.getReadableDatabase();
 
 		Cursor cursor = db.query(TABLE_ITEMS, new String[] { KEY_IDI,
 				KEY_INAME, KEY_DESC, KEY_MEMID, KEY_STATUS, KEY_RESOL,
 				KEY_TYPE, KEY_MONTH, KEY_DAY, KEY_YEAR, KEY_LOC }, KEY_IDI + "=?",
 				new String[] { String.valueOf(id) }, null, null, null, null);
-		if (cursor != null)
+		int memID = 0;
+		if (cursor != null){
 			cursor.moveToFirst();
-		int memID = Integer.parseInt(cursor.getString(3));
+			try{
+				memID = Integer.parseInt(cursor.getString(3));
+			}
+			catch(Exception e){
+				System.out.println("No member ID was inputted");
+				System.out.println(e.getStackTrace());
+			}
+		}
 		ArrayList<Member>  mem = Security.getDBM().getAllMembers();
 		Member temp = null;
 		for(Member m : mem){
@@ -130,16 +167,27 @@ public class DatabaseHandlerItems extends SQLiteOpenHelper {
 		else{
 			temp2 = true;
 		}
+		Item item = null;
+		try{
+			item = new Item(Integer.parseInt(cursor.getString(0)),
+					cursor.getString(1), cursor.getString(2), temp, temp1, 
+					temp2, cursor.getString(6), Integer.parseInt(cursor.getString(7)), 
+					Integer.parseInt(cursor.getString(8)), 
+					Integer.parseInt(cursor.getString(9)), cursor.getString(10));
+		}
+		catch(Exception e){
+			System.out.println("No member ID was inputted");
+			System.out.println(e.getStackTrace());
+		}
 		
-		Item item = new Item(Integer.parseInt(cursor.getString(0)),
-				cursor.getString(1), cursor.getString(2), temp, temp1, 
-				temp2, cursor.getString(6), Integer.parseInt(cursor.getString(7)), 
-				Integer.parseInt(cursor.getString(8)), Integer.parseInt(cursor.getString(9)), cursor.getString(10));
 		cursor.close();
 		db.close();
 		return item;
 	}
 	
+	/**
+	 * @return items - all the items in the database
+	 */
 	public ArrayList<Item> getAllItems(){
 		ArrayList<Item> items = new ArrayList<Item>();
 		
@@ -151,7 +199,14 @@ public class DatabaseHandlerItems extends SQLiteOpenHelper {
 		// looping through all rows and adding to list
 		if (cursor.moveToFirst()) {
 			do {
-				int memID = Integer.parseInt(cursor.getString(3));
+				int memID = 0;
+				try{
+					memID = Integer.parseInt(cursor.getString(3));
+				}
+				catch(Exception e){
+					System.out.println("No member ID was inputted");
+					System.out.println(e.getStackTrace());
+				}
 				ArrayList<Member>  mem = Security.getDBM().getAllMembers();
 				Member temp = null;
 				for(Member m : mem){
@@ -174,10 +229,20 @@ public class DatabaseHandlerItems extends SQLiteOpenHelper {
 					temp2 = true;
 				}
 								
-				Item item = new Item(Integer.parseInt(cursor.getString(0)),
-						cursor.getString(1), cursor.getString(2), temp, temp1, 
-						temp2, cursor.getString(6), Integer.parseInt(cursor.getString(7)), 
-						Integer.parseInt(cursor.getString(8)), Integer.parseInt(cursor.getString(9)), cursor.getString(10));
+				Item item = null;
+				try{
+					item = new Item(Integer.parseInt(cursor.getString(0)),
+							cursor.getString(1), cursor.getString(2), temp, temp1, 
+							temp2, cursor.getString(6), 
+							Integer.parseInt(cursor.getString(7)),
+							Integer.parseInt(cursor.getString(8)), 
+							Integer.parseInt(cursor.getString(9)), cursor.getString(10));
+				}
+				catch(Exception e){
+					System.out.println("No member ID was inputted");
+					System.out.println(e.getStackTrace());
+				}
+				
 				// Adding item to list
 				items.add(item);
 			} while (cursor.moveToNext());
@@ -187,30 +252,34 @@ public class DatabaseHandlerItems extends SQLiteOpenHelper {
 		return items;
 	}
 
-	public int getCurrentItemID()
-	{
-		ArrayList<Item> items = (ArrayList<Item>) getAllItems();
-		return items.size() +1;
+	/**
+	 * @return int - the current id to be assigned to the next added
+	 * item
+	 */
+	public int getCurrentItemID(){
+		ArrayList<Item> items = getAllItems();
+		return items.size() + 1;
 	}
 	
-	
-	void logItems()
-	{
+	/**
+	 * Logs all the items into the log
+	 */
+	public void logItems(){
 		Log.d("DatabaseHandler: ", "Inside Log Items()");
-		ArrayList<Item> items = (ArrayList<Item>) getAllItems();
-		for(Item i: items)
-		{
+		ArrayList<Item> items = getAllItems();
+		for(Item i: items){
 			Log.d("DatabaseHandler: ", i.toString());
 		}
 
 	}
-
 	
-	// Updating single item
+	/**
+	 * @param item - item that is being updated
+	 * @return int - 1 if database succesfully updated
+	 */
 	public int updateItem(Item item) {
 		SQLiteDatabase db = this.getWritableDatabase();
 		
-
 		ContentValues values = new ContentValues();
 		values.put(KEY_IDI, item.getID());
 		values.put(KEY_INAME, item.getName());
@@ -228,9 +297,11 @@ public class DatabaseHandlerItems extends SQLiteOpenHelper {
 		return db.update(TABLE_ITEMS, values, KEY_IDI + " = ?",
 				new String[] { String.valueOf(item.getID()) });
 	}
-
 	
-	// Deleting single place
+	/**
+	 * Deletes item from database
+	 * @param item - item to be deleted
+	 */
 	public void deleteItem(Item item) {
 		SQLiteDatabase db = this.getWritableDatabase();
 		db.delete(TABLE_ITEMS, KEY_IDI + " = ?",
@@ -238,7 +309,10 @@ public class DatabaseHandlerItems extends SQLiteOpenHelper {
 		db.close();
 	}
 	
-	// Getting member Count
+	/**
+	 * 
+	 * @return count of items in database
+	 */
 	public int getItemsCount() {
 		String countQuery = "SELECT  * FROM " + TABLE_ITEMS;
 		SQLiteDatabase db = this.getReadableDatabase();
