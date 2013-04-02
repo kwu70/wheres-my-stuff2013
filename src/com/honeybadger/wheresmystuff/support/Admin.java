f4af1043c177f170c2700556fd67731436a3b880
jelliott36 <jelliott36@gatech.edu>
2013-04-02 23:29:36 +0000

    Audited Admin, Item, and AdminSettingActivity
    Added javadoc and made it prettier apparently

--- src/com/honeybadger/wheresmystuff/support/Admin.java
+++ src/com/honeybadger/wheresmystuff/support/Admin.java
@@ -1,24 +1,27 @@
 package com.honeybadger.wheresmystuff.support;
 
-/*
+/**
  * This class is used to create Admins and for Views to access admin only methods
+ * 
+ *  @author Honey Badger
  */
 public class Admin extends Member {
 
 	
-	/*
+	/**
 	 * This constructor is a more detailed constructor and allows their 
 	 * to be a username for the admin
 	 * 
+	 * @param id - each member has id in database
 	 * @param email - email of new Admin
 	 * @param password - password of new Admin
 	 * @param name - name of new Admin
 	 */
-	public Admin(int ID, String email, String password, String name){
-		super(ID, email, password, name);
+	public Admin(int id, String email, String password, String name){
+		super(id, email, password, name);
 	}
 	
-	/*
+	/**
 	 * This method removes a member from the member list so that they may no
 	 * longer login
 	 * 
@@ -28,7 +31,7 @@ public class Admin extends Member {
 		Security.removeMember(mem.getEmail());
 	}
 	
-	/*
+	/**
 	 * This method creates a new admin and adds them to the
 	 * member list so they can login as an admin
 	 * 
@@ -39,11 +42,10 @@ public class Admin extends Member {
 		Security.addAdmin(email, password);
 	}
 	
-	
-	/*
+	/**
 	 * This method is used to unlock an account
 	 * 
-	 * @param mem- member that needs his/her account unlocked
+	 * @param mem Member that needs his/her account unlocked
 	 */
 	public void unlockAccount(Member mem){
 		Security.resetAttempts(mem);
--- src/com/honeybadger/wheresmystuff/views/AdminSettingActivity.java
+++ src/com/honeybadger/wheresmystuff/views/AdminSettingActivity.java
@@ -12,22 +12,41 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 
+/**
+ * Admin Setting Activity class is an activity for Admins that allows
+ * them to manipulate the system variables.
+ * Create a new admin
+ * Unlock members
+ * Remove members
+ * 
+ * @author Honey Badger
+ */
 public class AdminSettingActivity extends Activity{
 	
 	private String userEmail;
+	
 	private Intent memberActivity;
 	
 	//Textfields
 	private EditText adminEmail;
+	
 	private EditText adminPassword;
+	
 	private EditText adminConfirmPassword;
+	
 	private EditText email;
 	
 	//Dialog boxes
 	private AlertDialog.Builder builder;
+	
 	private AlertDialog.Builder remover;
+	
 	private AlertDialog.Builder unlocker;
 	
+	/**
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 * @param savedInstanceState
+	 */
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.admin_settings_view);
@@ -48,27 +67,39 @@ public class AdminSettingActivity extends Activity{
 		
 		//dialog box for Create Admin
 		builder = new AlertDialog.Builder(this);
-		builder.setMessage("Are you sure?").setPositiveButton("Yes", new DialogClickListener())
-	    .setNegativeButton("No", new DialogClickListener());
+		builder.setMessage("Are you sure?")
+		.setPositiveButton("Yes", new DialogClickListener())
+		.setNegativeButton("No", new DialogClickListener());
 		
 		//dialog box for remove Member
 		remover = new AlertDialog.Builder(this);
-		remover.setMessage("Are you sure?").setPositiveButton("Yes", new RemoverClickListener())
+		remover.setMessage("Are you sure?")
+		.setPositiveButton("Yes", new RemoverClickListener())
 	    .setNegativeButton("No", new RemoverClickListener());
 		
 		//dialog box for unlock member
 		unlocker = new AlertDialog.Builder(this);
-		unlocker.setMessage("Are you sure?").setPositiveButton("Yes", new UnlockerClickListener())
+		unlocker.setMessage("Are you sure?")
+		.setPositiveButton("Yes", new UnlockerClickListener())
 	    .setNegativeButton("No", new UnlockerClickListener());
 		
 		//listeners for buttons in admin_settings_view
-		findViewById(R.id.btnCreateAdmin).setOnClickListener(new CreateAdminOnClickListener());
-		findViewById(R.id.btnRemoveMember).setOnClickListener(new RemoveMemberOnClickListner());
-		findViewById(R.id.btnUnlockAccount).setOnClickListener(new UnlockMemberOnClickListner());
+		findViewById(R.id.btnCreateAdmin)
+		.setOnClickListener(new CreateAdminOnClickListener());
+		
+		findViewById(R.id.btnRemoveMember)
+		.setOnClickListener(new RemoveMemberOnClickListner());
+		
+		findViewById(R.id.btnUnlockAccount)
+		.setOnClickListener(new UnlockMemberOnClickListner());
 
 	}
 	
-	//When user presses back.
+	/**
+	 * Goes back to the last activity
+	 * 
+	 * @see android.app.Activity#onBackPressed()
+	 */
 	public void onBackPressed() {
 	  	startActivity(memberActivity);
 	}
@@ -77,8 +108,20 @@ public class AdminSettingActivity extends Activity{
 	 * Listener for Create Admin button.
 	 * Checks three textfields for valid user.
 	 * Works exactly the same as register.
+	 * 
+	 * @author Honey Badger
 	 */
 	private class CreateAdminOnClickListener implements OnClickListener{
+		
+		/**
+		 * When the user clicks create admin button it gets the three textfields
+		 * two to check user email and see if they are identical and the third
+		 * is the password for the admin
+		 * It creates the admin with these given parameters
+		 * 
+		 * @see android.view.View.OnClickListener#onClick(android.view.View)
+		 * @param v
+		 */
 		@Override
 		public void onClick(View v) {
 			//Strings from textfields
@@ -101,7 +144,7 @@ public class AdminSettingActivity extends Activity{
 			}
 			
 			// Check for a valid and non-duplicate email address.
-			if (TextUtils.isEmpty(email)) {						
+			if (TextUtils.isEmpty(email)){
 				adminEmail.setError(getString(R.string.error_field_required));
 				focusView = adminEmail;
 				cancel = true;
@@ -112,9 +155,9 @@ public class AdminSettingActivity extends Activity{
 			} else if (Security.contains(email)) {
 			// TODO: change error to "already existing email" error once XML updated
 				adminEmail.setError(getString(R.string.error_invalid_email));
-				focusView = adminEmail;					
+				focusView = adminEmail;
 				cancel = true;
-			}			
+			}
 			
 			if (cancel) {
 				// There was an error; don't attempt login and focus the first
@@ -138,9 +181,20 @@ public class AdminSettingActivity extends Activity{
 	
 	/**
 	 * Remove Member Listener is the listener for the Remove Member button.
-	 * If the member in the textfield exists then it is removed otherwise it throws an error
+	 * If the member in the textfield exists then it is removed
+	 * otherwise it throws an error
+	 * 
+	 * @author Honey Badger
 	 */
 	private class RemoveMemberOnClickListner implements OnClickListener{
+		
+		/**
+		 * When the user clicks on the remove member button
+		 * it removes the member from the database.
+		 * 
+		 * @see android.view.View.OnClickListener#onClick(android.view.View)
+		 * @param arg0
+		 */
 		@Override
 		public void onClick(View arg0) {
 			String removeEmail = email.getText().toString();
@@ -150,14 +204,24 @@ public class AdminSettingActivity extends Activity{
 			else{
 				email.setError(getString(R.string.error_invalid_email));
 			}
-		}	
+		}
 	}
 	
 	/**
 	 * Unlock Member Listener is the listener for the Unlock Member button
 	 * If the member in the textfield exists then their failedAttempts is reset to 0
+	 * 
+	 * @author Honey Badger
 	 */
 	private class UnlockMemberOnClickListner implements OnClickListener{
+		
+		/**
+		 * When the user clicks unlock is unlocks the member that is entered
+		 * into the text field
+		 * 
+		 * @see android.view.View.OnClickListener#onClick(android.view.View)
+		 * @param v
+		 */
 		@Override
 		public void onClick(View v) {
 			String unlockEmail = email.getText().toString();
@@ -176,13 +240,19 @@ public class AdminSettingActivity extends Activity{
 	 * 
 	 * This just brings up a box asking whether the user wants to create the Admin
 	 * if they do then the Admin is created.
+	 * 
+	 * @author Honey Badger
 	 */
 	private class DialogClickListener implements DialogInterface.OnClickListener{
-		/*
+		
+		/**
 		 * If the user clicks yes on the display box, the activity
 		 * creates a new member and starts the login activity
 		 * If they click no, then the message goes away  and nothing
 		 * happens
+		 * 
+		 * @param dialog
+		 * @param which
 		 */
 		@Override
 	    public void onClick(DialogInterface dialog, int which) {
@@ -196,6 +266,8 @@ public class AdminSettingActivity extends Activity{
 	        	break;
 	        case DialogInterface.BUTTON_NEGATIVE:
 	        	break;
+	        default:
+	        	break;
 	        }
 	    }
 		
@@ -205,9 +277,20 @@ public class AdminSettingActivity extends Activity{
 	 * Remove Click Listener is the Dialog for the Remove Member button
 	 * Yes or No dialog that asks if the user wants to remove the member from the system
 	 * If yes then the member will be removed from the system.
+	 * 
+	 * @author Honey Badger
 	 */
 	private class RemoverClickListener implements DialogInterface.OnClickListener{
 
+		/**
+		 * Checks if yes or no and then executes remove 
+		 * depending on the answer.
+		 * Yes - Removes user
+		 * No - Cancels
+		 * 
+		 * @param dialog
+		 * @param which
+		 */
 		@Override
 		public void onClick(DialogInterface dialog, int which) {
 			String userEmail = email.getText().toString();
@@ -219,6 +302,8 @@ public class AdminSettingActivity extends Activity{
 	        	break;
 	        case DialogInterface.BUTTON_NEGATIVE:
 	        	break;
+	        default:
+	        	break;
 	        }
 		}
 	}
@@ -227,8 +312,20 @@ public class AdminSettingActivity extends Activity{
 	 * Unlock Click Listener for Unlock Member button
 	 * Yes or No Dialog to check that the user wants to unlock the member
 	 * If Yes then the user unlocks the member by resetting their failedAttempts to 0.
+	 * 
+	 * @author Honey Badger
 	 */
 	private class UnlockerClickListener implements DialogInterface.OnClickListener{
+		
+		/**
+		 * Checks if yes or no and then executes unlock 
+		 * depending on the answer.
+		 * Yes - Unlocks user
+		 * No - Cancels
+		 * 
+		 * @param dialog
+		 * @param which
+		 */
 		@Override
 		public void onClick(DialogInterface dialog, int which) {
 			String userEmail = email.getText().toString();
@@ -239,6 +336,8 @@ public class AdminSettingActivity extends Activity{
 	        	finish();
 	        	break;
 	        case DialogInterface.BUTTON_NEGATIVE:
+	        	break;
+	        default:
 	        	break;
 	        }
 		}
