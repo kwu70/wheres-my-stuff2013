b2d3fb01774fc4e11172902660664410d0fd2728
jalex618 <jalex618@03b83ac3-6690-9d20-bea0-5b52570744bb>
2013-04-05 15:59:40 +0000

    Added search by name and location.

--- src/com/honeybadger/wheresmystuff/views/SearchActivity.java
+++ src/com/honeybadger/wheresmystuff/views/SearchActivity.java
@@ -1,98 +1,66 @@
-package com.honeybadger.wheresmystuff.views;
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:id="@+id/RelativeLayout1"
+    android:layout_width="fill_parent"
+    android:layout_height="fill_parent" >
 
-import com.honeybadger.wheresmystuff.R;
-import com.honeybadger.wheresmystuff.support.Search;
-import com.honeybadger.wheresmystuff.support.Item;
+    <Button
+        android:id="@+id/btnItemSearch"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_alignParentLeft="true"
+        android:text="@string/searchbutton_text" />
 
-import java.util.ArrayList;
-import android.app.Activity;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.RadioButton;
+    <ListView
+        android:id="@+id/search_listView"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:layout_alignParentLeft="true"
+        android:layout_alignParentRight="true"
+        android:layout_below="@+id/radioGroup1" >
 
-public class SearchActivity extends Activity{
+    </ListView>
 
-	private EditText s_editText;
+    <EditText
+        android:id="@+id/search_editText"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_alignBaseline="@+id/btnItemSearch"
+        android:layout_alignBottom="@+id/btnItemSearch"
+        android:layout_toRightOf="@+id/btnItemSearch"
+        android:ems="10"
+        android:inputType="text" >
 
-	//used in the ListViews 
-	private ArrayAdapter<String> adapterItems;
-	private ArrayAdapter<String> adapterTemp;
-	private ListView itemsList;
+        <requestFocus />
+    </EditText>
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.search_view);
+    <RadioGroup
+        android:id="@+id/radioGroup1"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_alignParentLeft="true"
+        android:layout_below="@+id/search_editText"
+        android:layout_marginLeft="19dp" >
 
-		//adapterItems is the Item List that is displayed on the screen
-		adapterItems = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1);
-		//temp for changing adapter item.
-		adapterTemp = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1);
+        <RadioButton
+            android:id="@+id/radioName"
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:checked="true"
+            android:text="@string/radioName" />
 
-		//item List array of strings that appears on screen
-		itemsList = (ListView) findViewById(R.id.search_listView);
-		itemsList.setAdapter(adapterItems);
-		
-		s_editText = (EditText) findViewById(R.id.search_editText);
+        <RadioButton
+            android:id="@+id/radioNameAndLocation"
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:text="@string/search_NameAndLocation" />
 
-		//Button listener for Search
-		findViewById(R.id.btnItemSearch).setOnClickListener(new SearchItemClickListener());
-	}
+        <RadioButton
+            android:id="@+id/radioCategory"
+            android:layout_width="wrap_content"
+            android:layout_height="wrap_content"
+            android:text="@string/radioCategory" />
 
-	/**
-	 * This class is a listener for the Search button and when it is clicked 
-	 * it searches the database based on the radio button (either name or category)
-	 * and produces a list below.
-	 */
-	private class SearchItemClickListener implements OnClickListener{
+    </RadioGroup>
 
-		@Override
-		public void onClick(View v) {
-			
-			adapterTemp.clear();
-			String searchCriteria = s_editText.getText().toString();
-
-			RadioButton name = (RadioButton)findViewById(R.id.radioName);
-			RadioButton category = (RadioButton)findViewById(R.id.radioCategory);
-
-			if(TextUtils.isEmpty(searchCriteria)) {
-				s_editText.setError(getString(R.string.error_field_required));
-				s_editText.requestFocus();
-				
-			}
-			else {
-				if(name.isChecked()){
-					//search by name
-					ArrayList<Item> tempNames = Search.searchByName(searchCriteria);
-					if(tempNames != null){
-						for(Item item: tempNames){
-							adapterTemp.add(item.getName());
-						}
-					}
-				} else if(category.isChecked()){
-					//search by category
-					ArrayList<Item> tempCategories = Search.searchByCategory(searchCriteria);
-					if(tempCategories != null){
-						for(Item item: tempCategories){
-							adapterTemp.add(item.getName());
-						}
-					}
-				}
-
-				adapterItems = adapterTemp;
-				itemsList.setAdapter(adapterItems);
-			}
-			
-		}
-	}
-}
-
-
-
+</RelativeLayout>
