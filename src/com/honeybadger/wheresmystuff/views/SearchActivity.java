a73b48b85a473b0cf88a78895ab25b2fe8c3a33b
jalex618 <jalex618@03b83ac3-6690-9d20-bea0-5b52570744bb>
2013-04-05 14:39:04 +0000

    Fixed the cause of SearchActivity crashing.

--- src/com/honeybadger/wheresmystuff/views/SearchActivity.java
+++ src/com/honeybadger/wheresmystuff/views/SearchActivity.java
@@ -1,36 +1,23 @@
 package com.honeybadger.wheresmystuff.views;
 
 import com.honeybadger.wheresmystuff.R;
-import com.honeybadger.wheresmystuff.support.Member;
 import com.honeybadger.wheresmystuff.support.Search;
 import com.honeybadger.wheresmystuff.support.Item;
 
 import java.util.ArrayList;
 import android.app.Activity;
 import android.os.Bundle;
-import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.AdapterView.OnItemClickListener;
 
 public class SearchActivity extends Activity{
 
-	private ListView listView1;
-
-	private EditText editText1;
-
-	//current user
-	private Member currentMember;
-
-	//intent to return to MemberActivity
-	private Intent returnIntent;
+	private EditText s_editText;
 
 	//used in the ListViews 
 	private ArrayAdapter<String> adapterItems;
@@ -38,16 +25,25 @@ public class SearchActivity extends Activity{
 	private ListView itemsList;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.search_view);
 
+		//adapterItems is the Item List that is displayed on the screen
+		adapterItems = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1);
+		//temp for changing adapter item.
+		adapterTemp = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1);
+
 		//item List array of strings that appears on screen
-		itemsList = (ListView) findViewById(R.id.listView1);
+		itemsList = (ListView) findViewById(R.id.search_listView);
 		itemsList.setAdapter(adapterItems);
+		
+		s_editText = (EditText) findViewById(R.id.search_editText);
 
 		//Button listener for Search
-		findViewById(R.id.btnSearchForItems).setOnClickListener(new SearchItemClickListener());
+		findViewById(R.id.btnItemSearch).setOnClickListener(new SearchItemClickListener());
 	}
 
 	/**
@@ -59,52 +55,43 @@ public class SearchActivity extends Activity{
 
 		@Override
 		public void onClick(View v) {
-			String searchCriteria = editText1.getText().toString();
 			
+			adapterTemp.clear();
+			String searchCriteria = s_editText.getText().toString();
 
 			RadioButton name = (RadioButton)findViewById(R.id.radioName);
 			RadioButton category = (RadioButton)findViewById(R.id.radioCategory);
 
-			if(name.isChecked()){
-				//search by name
-				ArrayList<Item> tempNames = Search.searchByName(searchCriteria);
-				for(Item item: tempNames){
-					adapterTemp.add(item.getName());
-				}
+			if(TextUtils.isEmpty(searchCriteria)) {
+				s_editText.setError(getString(R.string.error_field_required));
+				s_editText.requestFocus();
+				
 			}
-			if(category.isChecked()){
-				//search by category
-				ArrayList<Item> tempCategories = Search.searchByCategory(searchCriteria);
-				for(Item item: tempCategories){
-					adapterTemp.add(item.getName());
+			else {
+				if(name.isChecked()){
+					//search by name
+					ArrayList<Item> tempNames = Search.searchByName(searchCriteria);
+					if(tempNames != null){
+						for(Item item: tempNames){
+							adapterTemp.add(item.getName());
+						}
+					}
+				} else if(category.isChecked()){
+					//search by category
+					ArrayList<Item> tempCategories = Search.searchByCategory(searchCriteria);
+					if(tempCategories != null){
+						for(Item item: tempCategories){
+							adapterTemp.add(item.getName());
+						}
+					}
 				}
-			}
 
-			//gets currently selected search type
-			RadioGroup radioGroup = (RadioGroup)findViewById(R.id.radioGroup1);
-			int radioButtonID = radioGroup.getCheckedRadioButtonId();
-			RadioButton rad = (RadioButton) findViewById(radioButtonID);
-			String type = rad.getText().toString();
+				adapterItems = adapterTemp;
+				itemsList.setAdapter(adapterItems);
+			}
 			
-			adapterItems = adapterTemp;
-			itemsList.setAdapter(adapterItems);
-			
-			finish();
 		}
 	}
-	
-	/**
-	 * This method is called when the back button is pressed
-	 * and returns to the previous activity
-	 */
-	public void onBackPressed() {
-	    Intent i = new Intent(this, MemberActivity.class);
-	    startActivity(i);
-	    finish();
-	}
-	
-	
-
 }
 
 
