5e0946bda6d69280612f862ce6c066c1e746a160
stange.anthony <stange.anthony@gmail.com>
2013-04-09 17:51:32 +0000

    Got rid of some performance issues caught by Lint checks. They were just unused strings in the xml file

--- gen/com/honeybadger/wheresmystuff/R.java
+++ gen/com/honeybadger/wheresmystuff/R.java
@@ -15,7 +15,6 @@ public final class R {
     }
     public static final class dimen {
         public static final int ButtonDP=0x7f040000;
-        public static final int btnAccount=0x7f040001;
     }
     public static final class drawable {
         public static final int honey_badger=0x7f020000;
@@ -94,74 +93,57 @@ public final class R {
         public static final int activity_login=0x7f080000;
     }
     public static final class string {
-        public static final int account_preferences=0x7f050002;
-        public static final int action_sign_in_register=0x7f050038;
-        public static final int action_sign_in_short=0x7f050039;
-        public static final int add_item=0x7f050006;
-        public static final int add_item_view=0x7f050010;
-        public static final int add_lostfound_item=0x7f05001b;
-        public static final int admin_settings_view=0x7f050022;
+        public static final int action_sign_in_short=0x7f05002a;
+        public static final int add_item=0x7f050002;
+        public static final int add_item_view=0x7f050008;
+        public static final int add_lostfound_item=0x7f050013;
+        public static final int admin_settings_view=0x7f05001a;
         public static final int app_name=0x7f050000;
-        public static final int btn_admin=0x7f05001d;
-        public static final int btn_create=0x7f05001e;
-        public static final int btn_remove_member=0x7f050020;
-        public static final int btn_unlock_account=0x7f050021;
-        public static final int cancel=0x7f05001c;
-        public static final int category=0x7f05002f;
-        public static final int clothing_type=0x7f050017;
-        public static final int confirm_password=0x7f05000f;
-        public static final int create_account=0x7f05000a;
-        public static final int date_button=0x7f050024;
-        public static final int date_button2=0x7f050029;
-        public static final int donation_type=0x7f050019;
-        public static final int error_field_required=0x7f05003f;
-        public static final int error_incorrect_password=0x7f05003e;
-        public static final int error_incorrect_password_email=0x7f050040;
-        public static final int error_invalid_email=0x7f05003c;
-        public static final int error_invalid_password=0x7f05003d;
-        public static final int error_lock_out=0x7f05001a;
-        public static final int error_no_match=0x7f050041;
-        public static final int first_name=0x7f050007;
-        public static final int food_type=0x7f050016;
-        public static final int found_item_rad=0x7f050014;
-        public static final int found_items=0x7f050004;
-        public static final int found_items_list=0x7f05000b;
-        public static final int item_description=0x7f050012;
-        public static final int item_list=0x7f050027;
-        public static final int item_name=0x7f050011;
-        public static final int item_type_lbl=0x7f050015;
-        public static final int last_name=0x7f050008;
-        public static final int lblDate=0x7f050025;
-        public static final int lbl_filter=0x7f050028;
-        public static final int lbl_filter2=0x7f05002a;
-        public static final int lbl_filter_by_date=0x7f05002b;
-        public static final int lbl_item_list=0x7f050026;
-        public static final int lbl_userEmail=0x7f05001f;
-        public static final int login_progress_signing_in=0x7f05003b;
-        public static final int lost_item_rad=0x7f050013;
-        public static final int lost_items=0x7f050005;
-        public static final int lost_items_list=0x7f05000c;
+        public static final int btn_admin=0x7f050015;
+        public static final int btn_create=0x7f050016;
+        public static final int btn_remove_member=0x7f050018;
+        public static final int btn_unlock_account=0x7f050019;
+        public static final int cancel=0x7f050014;
+        public static final int clothing_type=0x7f05000f;
+        public static final int confirm_password=0x7f050007;
+        public static final int create_account=0x7f050004;
+        public static final int date_button2=0x7f05001f;
+        public static final int donation_type=0x7f050011;
+        public static final int error_field_required=0x7f05002e;
+        public static final int error_incorrect_password_email=0x7f05002f;
+        public static final int error_invalid_email=0x7f05002c;
+        public static final int error_invalid_password=0x7f05002d;
+        public static final int error_lock_out=0x7f050012;
+        public static final int error_no_match=0x7f050030;
+        public static final int food_type=0x7f05000e;
+        public static final int found_item_rad=0x7f05000c;
+        public static final int item_description=0x7f05000a;
+        public static final int item_list=0x7f05001d;
+        public static final int item_name=0x7f050009;
+        public static final int item_type_lbl=0x7f05000d;
+        public static final int lblDate=0x7f05001c;
+        public static final int lbl_filter=0x7f05001e;
+        public static final int lbl_filter2=0x7f050020;
+        public static final int lbl_filter_by_date=0x7f050021;
+        public static final int lbl_userEmail=0x7f050017;
+        public static final int lost_item_rad=0x7f05000b;
         public static final int member_activity=0x7f050001;
-        public static final int menu_forgot_password=0x7f05003a;
-        public static final int personal_type=0x7f050018;
-        public static final int prompt=0x7f050023;
+        public static final int menu_forgot_password=0x7f05002b;
+        public static final int personal_type=0x7f050010;
+        public static final int prompt=0x7f05001b;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f050036;
-        public static final int prompt_password=0x7f050037;
-        public static final int radioCategory=0x7f050033;
-        public static final int radioName=0x7f050031;
-        public static final int register=0x7f050009;
-        public static final int register_email=0x7f05000d;
-        public static final int register_password=0x7f05000e;
-        public static final int search_NameAndLocation=0x7f050035;
-        public static final int search_activity=0x7f05002e;
-        public static final int search_for_items=0x7f050034;
-        public static final int search_hint=0x7f05002d;
-        public static final int search_label=0x7f05002c;
-        public static final int search_radio_category=0x7f050032;
-        public static final int search_radio_name=0x7f050003;
-        public static final int searchbutton_text=0x7f050030;
+        public static final int prompt_email=0x7f050028;
+        public static final int prompt_password=0x7f050029;
+        public static final int radioCategory=0x7f050025;
+        public static final int radioName=0x7f050024;
+        public static final int register=0x7f050003;
+        public static final int register_email=0x7f050005;
+        public static final int register_password=0x7f050006;
+        public static final int search_NameAndLocation=0x7f050027;
+        public static final int search_activity=0x7f050022;
+        public static final int search_for_items=0x7f050026;
+        public static final int searchbutton_text=0x7f050023;
     }
     public static final class style {
         /** 
