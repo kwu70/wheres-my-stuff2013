d61747a7c39286e85647c25c7c6c05e941965e7b
thewilsonkid <thewilsonkid@03b83ac3-6690-9d20-bea0-5b52570744bb>
2013-04-10 17:04:39 +0000

    fixed some stuff for audit

--- WheresMyStuffTestTest/src/com/honeybadger/wheresmystuff/test/JayneTestCase.java
+++ WheresMyStuffTestTest/src/com/honeybadger/wheresmystuff/test/JayneTestCase.java
@@ -12,14 +12,24 @@ import com.honeybadger.wheresmystuff.views.LoginView;
 * JUnit Test for the filterDate Method in Search.
 * The method filter's a member's items to show only those 
 * added on or before a specified date.
+* 
+* @author HoneyBadger - Jayne
+* @version 1.0
 */
 public class JayneTestCase extends android.test.ActivityInstrumentationTestCase2<LoginView>{
 	
+	//new instance of LoginView
 	private LoginView lv;
+	
+	//new member to add in order to test the method
 	private Member member;
+	
+	//instances of Item to use in the test
 	private Item item1, item2, item3, item4;
 	
-	//constructor required for testing
+	/**
+	 * Constructor required for testing.
+	 */
 	public JayneTestCase(){
 		super(LoginView.class);
 	}
@@ -39,7 +49,7 @@ public class JayneTestCase extends android.test.ActivityInstrumentationTestCase2
 	* with null date to an empty list.
 	*/
 	public void testFilterDate1(){
-		List<Item> list = Search.filterDate(member, null);
+		final List<Item> list = Search.filterDate(member, null);
 		Boolean result = false;
 		
 		if(list.size() == 0){
@@ -54,7 +64,7 @@ public class JayneTestCase extends android.test.ActivityInstrumentationTestCase2
 	* with an empty string inputted for the date to an empty list
 	*/
 	public void testfilterDate2(){
-		List<Item> list = Search.filterDate(member, "");
+		final List<Item> list = Search.filterDate(member, "");
 		Boolean result = false;
 		
 		if(list.size() == 0){
@@ -63,27 +73,27 @@ public class JayneTestCase extends android.test.ActivityInstrumentationTestCase2
 		
 		assertTrue(result);
 	}
+	
 	/**
 	*Tests the filterDate method by comparing a filtered list
 	* of items added on or after "04/01/2013"
 	*/
 	public void testfilterDate3(){
 		item1 = new Item(Security.getCurrentID(), "item1", "test on date", member,
-						false, false, "personal", 04, 01, 2013, "Atlanta");
+						false, false, "personal", 4, 10, 2013, "Atlanta");
 		item2 = new Item(Security.getCurrentID(), "item2", "test on date", member,
-						false, false, "personal", 04, 01, 2013, "Atlanta");
+						false, false, "personal", 4, 10, 2013, "Atlanta");
 		item3 = new Item(Security.getCurrentID(), "item3", "test before date", member,
-						false, false, "personal", 03, 31, 2013, "Atlanta");
+						false, false, "personal", 3, 31, 2013, "Atlanta");
 		item4 = new Item(Security.getCurrentID(), "item4", "test after date", member,
-						false, false, "personal", 04, 02, 2013, "Atlanta");
+						false, false, "personal", 5, 2, 2013, "Atlanta");
 		
 		Security.addItem(item1);
 		Security.addItem(item2);
 		Security.addItem(item3);
 		Security.addItem(item4);
 		
-		List<Item> allItems = Security.getMemberItemList(member);
-		List<Item> filteredItems = Search.filterDate(member, "04/10/2013");
+		final List<Item> filteredItems = Search.filterDate(member, "04/10/2013");
 	
 		Boolean result = false;
 	
